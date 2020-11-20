983d2362185d41134692d8355f641e242f26f44e
Biniyam <abrandnewb@gmail.com>
2020-11-20 23:57:50 +0100

    1st commit

---
+++ skarp/js/script.js
@@ -0,0 +1,20 @@
+var tigrai = document.querySelector("#tigrai");
+var afar = document.querySelector("#afar");
+var amhara = document.querySelector("#amhara");
+var benishangul = document.querySelector("#benishangul");
+
+tigrai.addEventListener("click", () => {
+    alert("tigrai");
+});
+
+afar.addEventListener("click", () => {
+    alert("afar");
+});
+
+amhara.addEventListener("click", () => {
+    alert("amhara");
+});
+
+benishangul.addEventListener("click", () => {
+    alert("benishangul");
+});
